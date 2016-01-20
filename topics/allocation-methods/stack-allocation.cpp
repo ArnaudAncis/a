5da35d6b5bd9b107ad0173a80d35e2dacd97c256
Frederic Vogels <frederic.vogels@gmail.com>
2016-01-20 17:54:22 +0100

    Allocation method slides

---
+++ topics/allocation-methods/stack-allocation.cpp
@@ -0,0 +1,4 @@
+int* `\NODE{\tt a}{var a}` = new int`\NODE{[4]}{mem a}`;
+double* `\NODE{\tt b}{var b}` = new double`\NODE{[3]}{mem b}`;
+bool* `\NODE{\tt c}{var c}` = new bool`\NODE{[4]}{mem c}`;
+char* `\NODE{\tt d}{var  d}` = new char`\NODE{[12]}{mem d}`;
