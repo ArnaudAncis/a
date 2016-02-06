963159f100b8963a40bd63e19e3fd4d75db4d637
Frederic Vogels <frederic.vogels@gmail.com>
2016-02-07 00:53:50 +0100

    Preprocessor slides updates

--- topics/preprocessor/forrange.cpp
+++ topics/preprocessor/forrange.cpp
@@ -1,6 +1,6 @@
-#define FORRANGE(i, from, to) \
+#define FOR_RANGE(i, from, to) \
   for ( int i = from; i <= to; ++i )
 
-FORRANGE(i, 1, 10) {
+FOR_RANGE(i, 1, 10) {
   std::cout << i << std::endl;
 }
