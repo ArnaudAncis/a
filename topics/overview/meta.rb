19967324b78c9773d2b99e4a50952cab995672ff
Frederic Vogels <frederic.vogels@gmail.com>
2016-02-06 20:21:05 +0100

    Overview gets generated

--- topics/overview/meta.rb
+++ topics/overview/meta.rb
@@ -1,16 +1,21 @@
 require 'MetaData'
+require 'Html'
 require 'Upload'
 
 
 meta_object do
   extend MetaData::Actions
-  extend LaTeX::Actions
+  extend Html::Actions
   extend Upload::Mixin
 
   def remote_directory
     world.parent.remote_directory
   end
 
+  html_template('overview', group_name: 'overview')
+  
   uploadable('overview.html', 'ucll.css')
   upload_action
+
+  group_action(:full, [:overview, :upload])
 end
