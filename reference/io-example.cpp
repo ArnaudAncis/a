2b2838db26277b9e203752cfb33e3489b50416e8
Frederic Vogels <frederic.vogels@gmail.com>
2016-02-07 11:25:29 +0100

    Beginnings of reference

---
+++ reference/io-example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Enter your name: ";
+
+  std::string name;
+  std::cin >> name;
+
+  std::cout << "Hello " << name << std::endl;
+}
---
+++ reference/meta.rb
@@ -0,0 +1,24 @@
+require 'MetaData'
+require 'LaTeX'
+require 'Upload'
+
+
+meta_object do
+  extend MetaData::Actions
+  extend LaTeX::Actions
+  extend Upload::Mixin
+
+  tex_files = [ 'reference.tex' ]
+  tex_paths = tex_files.map { |tex_file| Pathname.new tex_file }
+  tex_actions(*tex_files, group_name: :tex)
+  pdf_paths = tex_paths.map { |tex| tex.sub_ext '.pdf' }
+
+  def remote_directory
+    world.parent.remote_directory
+  end
+
+  uploadable(*pdf_paths)
+  upload_action
+
+  group_action(:full, [:tex, :upload])
+end
