09d16dd82dfa2d3d6c3e13d28145ed14cffd925c
Frederic Vogels <frederic.vogels@gmail.com>
2016-02-11 10:52:36 +0100

    Update allocation-methods

--- topics/allocation-methods/stack-allocation.cpp
+++ topics/allocation-methods/stack-allocation.cpp
@@ -1,4 +1,3 @@
-int* `\NODE{\tt a}{var a}` = new int`\NODE{[4]}{mem a}`;
-double* `\NODE{\tt b}{var b}` = new double`\NODE{[3]}{mem b}`;
-bool* `\NODE{\tt c}{var c}` = new bool`\NODE{[4]}{mem c}`;
-char* `\NODE{\tt d}{var  d}` = new char`\NODE{[12]}{mem d}`;
+int* `\NODE{\tt a}{var a}` = new int`\NODE{[5]}{mem a}`;
+int `\NODE{\tt n}{var n}` = AskUserForInt();
+double* `\NODE{\tt b}{var b}` = new double`\NODE{[n]}{mem b}`;
