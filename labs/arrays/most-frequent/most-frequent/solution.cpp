79d419ce823196704afc31671c8c8e798c4ae992
Frederic Vogels <frederic.vogels@gmail.com>
2016-03-10 18:45:14 +0100

    labs/arrays
    Updated most-frequent tests

--- labs/arrays/most-frequent/most-frequent/solution.cpp
+++ labs/arrays/most-frequent/most-frequent/solution.cpp
@@ -92,14 +92,14 @@ void test(const std::vector<int>& ns, int expected)
     REQUIRE(expected == actual);
 }
 
-#define TEST(expected, ...) TEST_CASE("most_frequent on { " #__VA_ARGS__ " }, expected=" #expected, "[most_frequent]") { std::vector<int> ns = { __VA_ARGS__}; test(ns, expected); }
+#define TEST(expected, ...) TEST_CASE("most_frequent on " #__VA_ARGS__ ", expected=" #expected, "[most_frequent]") { std::vector<int> ns = __VA_ARGS__; test(ns, expected); }
 
-TEST(-1)
-TEST(0, 0)
-TEST(1, 1)
-TEST(2, 2)
-TEST(1, 0, 1, 1)
-TEST(0, 1, 0, 0)
-TEST(1, 0, 1, 2, 1)
-TEST(3, 1, 2, 3, 3, 4, 5)
-TEST(3, 3, 1, 2, 3, 4, 5)
+TEST(-1, {})
+TEST(0, { 0 })
+TEST(1, { 1 })
+TEST(2, { 2 })
+TEST(1, { 0, 1, 1 })
+TEST(0, { 1, 0, 0 })
+TEST(1, { 0, 1, 2, 1 })
+TEST(3, { 1, 2, 3, 3, 4, 5 })
+TEST(3, { 3, 1, 2, 3, 4, 5 })
