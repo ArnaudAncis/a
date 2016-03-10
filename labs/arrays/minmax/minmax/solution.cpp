aa6db6a3c6ba51df6b32eeefc0416117fe4be253
Frederic Vogels <frederic.vogels@gmail.com>
2016-03-10 18:43:29 +0100

    labs/arrays
    Updated minmax tests

--- labs/arrays/minmax/minmax/solution.cpp
+++ labs/arrays/minmax/minmax/solution.cpp
@@ -88,28 +88,28 @@ void vector_test(const std::vector<int>& ns, int expected_min, int expected_max)
     REQUIRE(max == expected_max);
 }
 
-#define C_STYLE_TEST(expected_min, expected_max, ...) TEST_CASE("minmax on C-style array { " #__VA_ARGS__ " }, expected min=" #expected_min ", max=" #expected_max, "[minmax]") { int ns[] = { __VA_ARGS__ }; c_style_test(ns, sizeof(ns) / sizeof(int), expected_min, expected_max); }
-#define CPP_STYLE_TEST(expected_min, expected_max, ...) TEST_CASE("minmax on C++-style array { " #__VA_ARGS__ " }, expected min=" #expected_min ", max=" #expected_max, "[minmax]") { int arr[] = { __VA_ARGS__ }; std::array<int, sizeof(arr)/sizeof(int)> ns = { __VA_ARGS__ }; cpp_style_test(ns, expected_min, expected_max); }
-#define VECTOR_TEST(expected_min, expected_max, ...) TEST_CASE("minmax on vector { " #__VA_ARGS__ " }, expected min=" #expected_min ", max=" #expected_max, "[minmax]") { std::vector<int> ns = { __VA_ARGS__ }; vector_test(ns, expected_min, expected_max); }
+#define C_STYLE_TEST(expected_min, expected_max, ...) TEST_CASE("minmax on C-style array " #__VA_ARGS__ ", expected min=" #expected_min ", max=" #expected_max, "[minmax]") { int ns[] = __VA_ARGS__; c_style_test(ns, sizeof(ns) / sizeof(int), expected_min, expected_max); }
+#define CPP_STYLE_TEST(expected_min, expected_max, ...) TEST_CASE("minmax on C++-style array " #__VA_ARGS__ ", expected min=" #expected_min ", max=" #expected_max, "[minmax]") { int arr[] = __VA_ARGS__; std::array<int, sizeof(arr)/sizeof(int)> ns = __VA_ARGS__; cpp_style_test(ns, expected_min, expected_max); }
+#define VECTOR_TEST(expected_min, expected_max, ...) TEST_CASE("minmax on vector " #__VA_ARGS__ ", expected min=" #expected_min ", max=" #expected_max, "[minmax]") { std::vector<int> ns = __VA_ARGS__; vector_test(ns, expected_min, expected_max); }
 
 
-C_STYLE_TEST(1, 1, 1)
-C_STYLE_TEST(1, 1, 1, 1)
-C_STYLE_TEST(1, 2, 1, 2)
-C_STYLE_TEST(1, 2, 2, 1)
-C_STYLE_TEST(0, 2, 2, 0, 1)
-C_STYLE_TEST(1, 9, 5, 7, 1, 5, 8, 4, 6, 2, 3, 8, 7, 4, 9)
+C_STYLE_TEST(1, 1, { 1 })
+C_STYLE_TEST(1, 1, { 1, 1 })
+C_STYLE_TEST(1, 2, { 1, 2 })
+C_STYLE_TEST(1, 2, { 2, 1 })
+C_STYLE_TEST(0, 2, { 2, 0, 1 })
+C_STYLE_TEST(1, 9, { 5, 7, 1, 5, 8, 4, 6, 2, 3, 8, 7, 4, 9 })
 
-CPP_STYLE_TEST(1, 1, 1)
-CPP_STYLE_TEST(1, 1, 1, 1)
-CPP_STYLE_TEST(1, 2, 1, 2)
-CPP_STYLE_TEST(1, 2, 2, 1)
-CPP_STYLE_TEST(0, 2, 2, 0, 1)
-CPP_STYLE_TEST(1, 9, 5, 7, 1, 5, 8, 4, 6, 2, 3, 8, 7, 4, 9)
+CPP_STYLE_TEST(1, 1, { 1 })
+CPP_STYLE_TEST(1, 1, { 1, 1 })
+CPP_STYLE_TEST(1, 2, { 1, 2 })
+CPP_STYLE_TEST(1, 2, { 2, 1 })
+CPP_STYLE_TEST(0, 2, { 2, 0, 1 })
+CPP_STYLE_TEST(1, 9, { 5, 7, 1, 5, 8, 4, 6, 2, 3, 8, 7, 4, 9 })
 
-VECTOR_TEST(1, 1, 1)
-VECTOR_TEST(1, 1, 1, 1)
-VECTOR_TEST(1, 2, 1, 2)
-VECTOR_TEST(1, 2, 2, 1)
-VECTOR_TEST(0, 2, 2, 0, 1)
-VECTOR_TEST(1, 9, 5, 7, 1, 5, 8, 4, 6, 2, 3, 8, 7, 4, 9)
+VECTOR_TEST(1, 1, { 1 })
+VECTOR_TEST(1, 1, { 1, 1 })
+VECTOR_TEST(1, 2, { 1, 2 })
+VECTOR_TEST(1, 2, { 2, 1 })
+VECTOR_TEST(0, 2, { 2, 0, 1 })
+VECTOR_TEST(1, 9, { 5, 7, 1, 5, 8, 4, 6, 2, 3, 8, 7, 4, 9 })
