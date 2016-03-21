4e42e219f757ca58be84addebdc4af38c8d94523
Frederic Vogels <frederic.vogels@ucll.be>
2016-03-21 13:38:46 +0100

    labs/operator-overloading
    Fixed error in slides

--- topics/operator-overloading/cin-cout.cpp
+++ topics/operator-overloading/cin-cout.cpp
@@ -1,2 +1,2 @@
-std::istream& operator <<(std::istream& in, T x);
-std::ostream& operator >>(std::ostream& out, T x);
+std::istream& operator >>(std::istream& in, T x);
+std::ostream& operator <<(std::ostream& out, T x);
