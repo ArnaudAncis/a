7e3dfe2bfc7e7e3a47824e6bf9bb8a768c55670e
Frederic Vogels <frederic.vogels@gmail.com>
2016-04-30 14:35:59 +0200

    exams

--- exams/meta.rb
+++ exams/meta.rb
@@ -8,13 +8,13 @@ meta_object do
   extend LaTeX::Actions
   extend Upload::Mixin
 
-  tex_files = [ 'exam.tex' ]
+  tex_files = [ 'with-solutions.tex', 'without-solutions.tex' ]
   tex_paths = tex_files.map { |tex_file| Pathname.new tex_file }
   tex_actions(*tex_files, group_name: :tex)
   pdf_paths = tex_paths.map { |tex| tex.sub_ext '.pdf' }
 
   def remote_directory
-    world.parent.remote_directory + 'reference'
+    world.parent.remote_directory + 'exams'
   end
 
   uploadable(*pdf_paths)
