45a4c620928bd0819234ec070867c901ba1cb009
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-07 19:15:21 +0200

    labs/huffman

--- labs/huffman/meta.rb
+++ labs/huffman/meta.rb
@@ -1,7 +1,8 @@
 require 'MetaData'
 require 'Html'
 require 'Code'
-require 'Cpp'
+require 'LaTeX'
+require 'Image'
 require 'Upload'
 require 'Html'
 require 'Environment'
@@ -24,6 +25,8 @@ end
 meta_object do
   extend MetaData::Actions
   extend Html::Actions
+  extend LaTeX::Actions
+  extend Image::Actions
   extend Upload::Mixin
 
   def remote_directory
@@ -31,9 +34,13 @@ meta_object do
   end
 
   html_template('assignment', context: Context.new, group_name: 'html')
+
+  tex_to_png2(*Dir['*.tex'], group_name: :img)
+
   
   uploadable('assignment.html')
   upload_action
 
-  group_action(:full, [:img, :html, :upload])
+  group_action(:all, [:img, :html])
+  group_action(:full, [:all, :upload])
 end
