fd29661d986b08dd27757264864cebf355fc9ffc
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-07 17:33:34 +0200

    labs/huffman

---
+++ labs/huffman/meta.rb
@@ -0,0 +1,39 @@
+require 'MetaData'
+require 'Html'
+require 'Code'
+require 'Cpp'
+require 'Upload'
+require 'Html'
+require 'Environment'
+require 'pathname'
+require 'fileutils'
+require '../shared.rb'
+
+module Quiz
+  extend Html::Generation::Quiz
+end
+
+
+class Context < SharedContext
+  include Contracts::TypeChecking
+  include Html::Generation
+end
+
+
+
+meta_object do
+  extend MetaData::Actions
+  extend Html::Actions
+  extend Upload::Mixin
+
+  def remote_directory
+    world.parent.remote_directory + Pathname.pwd.basename.to_s
+  end
+
+  html_template('assignment', context: Context.new, group_name: 'html')
+  
+  uploadable('assignment.html')
+  upload_action
+
+  group_action(:full, [:img, :html, :upload])
+end
