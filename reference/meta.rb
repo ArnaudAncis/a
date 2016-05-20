c1925fd10db6bc15b9758d085322c4c580f639f4
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-20 17:36:05 +0200

    reference/meta.rb

--- reference/meta.rb
+++ reference/meta.rb
@@ -3,18 +3,17 @@ require 'Dynamic'
 require 'MetaData2'
 require 'LaTeX2'
 require 'Upload2'
+require 'Shortcuts'
 
 
 meta_object do
   extend MetaData2
   extend LaTeX2::Actions
   extend Upload2::Actions
-  
+  extend Shortcuts::Actions
+
   inherit_remote_directory 'reference'
 
-  pdf_action = tex_actions('full-edition.tex', 'limited-edition.tex')
-  
-  uploadable(*pdf_action.child_actions.map(&:output_path))
-
-  bind({ :upload => upload_action })
+  quick_tex('full-edition.tex', 'limited-edition.tex')
+  quick_all(:pdf, :upload)
 end
