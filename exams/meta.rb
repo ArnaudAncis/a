354c72c88611dbedddb178fe92da6310c2930575
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-26 09:49:05 +0200

    exams/meta

--- exams/meta.rb
+++ exams/meta.rb
@@ -1,8 +1,9 @@
-require 'MetaData'
-require 'LaTeX'
-require 'Upload'
-require 'Template'
+require 'MetaData2'
+require 'LaTeX2'
+require 'Template2'
+require 'Shortcuts'
 require 'Cpp'
+
 
 class Context
   include Contracts::TypeChecking
@@ -55,32 +56,19 @@ end
 
 
 meta_object do
-  extend MetaData::Actions
-  extend LaTeX::Actions
-  extend Upload::Mixin
-  extend Template::Actions
+  extend MetaData2
+  extend LaTeX2::Actions
+  extend Template2::Actions
+  extend Shortcuts::Actions
 
+  template_actions = Dir['*.template'].map do |template_file|
+    template(input: template_file, context: Context.new)
+  end
+  bind( { :tex => make_sequence(*template_actions, description: 'Processes templates') } )
+  
   
   tex_files = [ 'with-solutions.tex', 'without-solutions.tex' ]
-  tex_paths = tex_files.map { |tex_file| Pathname.new tex_file }
-  tex_actions(*tex_files, group_name: :pdf)
-  pdf_paths = tex_paths.map { |tex| tex.sub_ext '.pdf' }
+  tex_actions(*tex_files, action_name: :pdf)
 
-  action_names = Dir['*.template'].map do |template_name|
-    template_path = Pathname.new template_name
-    output_path = template_path.basename '.template'
-    template_generate(template_name, output_path.to_s, context: Context.new) # Returns action_name
-  end
-
-  group_action(:tex, action_names)
-  
-  def remote_directory
-    world.parent.remote_directory + 'exams'
-  end
-
-  uploadable(*pdf_paths)
-  upload_action
-
-  group_action(:all, [:tex, :pdf])
-  group_action(:full, [:tex, :pdf, :upload])
+  quick_all( :tex, :pdf )
 end
