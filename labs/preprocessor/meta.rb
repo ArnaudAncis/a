3f53994120603e1df06ef6ac413e29b7952e6a3a
Frederic Vogels <frederic.vogels@gmail.com>
2017-02-12 15:25:33 +0100

    labs/preprocessor

--- labs/preprocessor/meta.rb
+++ labs/preprocessor/meta.rb
@@ -1,8 +1,9 @@
-require 'MetaData'
-require 'Html'
+require 'MetaData2'
+require 'Html2'
+require 'Template2'
 require 'Code'
 require 'Cpp'
-require 'Upload'
+require 'Upload2'
 require 'Html'
 require 'Environment'
 require 'pathname'
@@ -117,19 +118,16 @@ end
 
 
 meta_object do
-  extend MetaData::Actions
-  extend Html::Actions
-  extend Upload::Mixin
+  extend MetaData2
+  extend Template2::Actions
+  extend Upload2::Actions
 
   def remote_directory
     world.parent.remote_directory + Pathname.pwd.basename.to_s
   end
 
-  html_template('assignment', context: Context.new, group_name: 'html')
+  bind( { :html => template(input: 'assignment.html.template', context: Context.new) } )
 
   uploadable('assignment.html')
   uploadable('split-solution.txt')
-  upload_action
-
-  group_action(:full, [:html, :upload])
 end
