cbd81cf6be5534c4efe7677f37dfbd5f312634ca
Frederic Vogels <frederic.vogels@gmail.com>
2017-03-12 15:39:57 +0100

    labs/references

---
+++ labs/references/coding-exercises/solution/nth/nth.cpp
@@ -0,0 +1,16 @@
+#include "nth.h"
+#include <assert.h>
+
+
+int& nth(std::vector<int>& ns, int index)
+{
+    index = index % int(ns.size());
+    index += ns.size();
+    index = index % int(ns.size());
+
+    // Sanity checks
+    assert(0 <= index);
+    assert(0 < ns.size());
+
+    return ns[index];
+}
---
+++ labs/references/coding-exercises/solution/nth/nth.h
@@ -0,0 +1,9 @@
+#ifndef NTH_H
+#define NTH_H
+
+#include <vector>
+
+
+int& nth(std::vector<int>&, int);
+
+#endif
