6980a888950fdebe29cbc55c6f766d521acc4a36
Frederic Vogels <frederic.vogels@gmail.com>
2017-04-13 23:35:43 +0200

    labs/meta.rb

--- labs/meta.rb
+++ labs/meta.rb
@@ -1,10 +1,18 @@
-require 'MetaData'
-require 'Upload'
+require 'MetaData2'
+require 'Upload2'
+require 'Shortcuts'
 
 
 meta_object do
-  extend MetaData::Actions
-  extend Upload::Mixin
+  extend MetaData2
+  extend Upload2::Actions
+  extend Shortcuts::Actions
 
-  inherit_remote_directory('labs')
+
+  inherit_remote_directory 'labs'
+
+  quick_recursive_all
+
+  register_transfer( Pathname.new('pvm.css').expand_path,
+                     Pathname.new('/var/www/courses/shared/pvm.css') )
 end
