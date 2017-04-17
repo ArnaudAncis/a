219907d49b6480af1db93afb5da6a8ab20f38c89
Frederic Vogels <frederic.vogels@gmail.com>
2017-04-17 20:36:18 +0200

    labs/sound

---
+++ labs/sound/solution/app.cpp
@@ -0,0 +1,150 @@
+#ifndef TEST_BUILD
+#include "streams/bytes-buffer.h"
+#include "streams/normaliser.h"
+#include "streams/conversions.h"
+#include "streams/multiplexer.h"
+#include "waves/wave.h"
+#include "waves/sine-wave.h"
+#include "waves/wave-sampling-stream.h"
+#include "waves/wave-concatenation.h"
+#include "waves/interpolator.h"
+#include "waves/speeder-upper.h"
+#include "waves/zero-wave.h"
+#include "wave-file.h"
+#include "note.h"
+#include "composer.h"
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <assert.h>
+
+
+Wave reverb(Wave wave, unsigned n_reverberations, double factor, double delay)
+{
+    Wave result = zero_wave(0);
+
+    for (unsigned i = 0; i != n_reverberations; ++i)
+    {
+        Wave reverberation = (wave * pow(factor, i)) >> (i * delay);
+        result = result + reverberation;
+    }
+
+    return result;
+}
+
+void bach_mono()
+{
+    WAVE_DATA wave_data;
+
+    auto result_wave = treble() + bass();
+
+    auto double_samples = normalise(sample_wave(result_wave, 44100));
+    auto int16_samples = convert_double_to_int16_stream(double_samples);
+    auto output_stream = convert_int16_to_uint8_stream(int16_samples);
+
+    wave_data.bits_per_sample = 16;
+    wave_data.n_channels = 1;
+    wave_data.sample_rate = 44100;
+    wave_data.stream = output_stream;
+
+    write_wave_file("e:/temp/wave/output.wav", wave_data);
+}
+
+void bach_stereo()
+{
+    WAVE_DATA wave_data;
+
+    auto result_left = treble();
+    auto result_right = bass();
+
+    auto double_samples_left = normalise(sample_wave(result_left, 44100));
+    auto double_samples_right = normalise(sample_wave(result_right, 44100));
+
+    auto int16_samples_left = convert_double_to_int16_stream(double_samples_left);
+    auto int16_samples_right = convert_double_to_int16_stream(double_samples_right);
+
+    auto stereo_samples = multiplex(int16_samples_left, int16_samples_right);
+    auto output_stream = convert_int16_to_uint8_stream(stereo_samples);
+
+    wave_data.bits_per_sample = 16;
+    wave_data.n_channels = 2;
+    wave_data.sample_rate = 44100;
+    wave_data.stream = output_stream;
+
+    write_wave_file("e:/temp/wave/output.wav", wave_data);
+}
+
+void convert_8bit_file()
+{
+    WAVE_DATA wave_data;
+
+    read_wave_file("e:/temp/wave/input-8bit-44-mono.wav", &wave_data);
+
+    if (wave_data.n_channels != 1 || wave_data.sample_rate != 44100 || wave_data.bits_per_sample != 8)
+    {
+        std::cerr << "Unsupported format" << std::endl;
+        std::cerr << "Channels: " << wave_data.n_channels << std::endl;
+        std::cerr << "Sample rate: " << wave_data.sample_rate << std::endl;
+        std::cerr << "Bits per sample: " << wave_data.bits_per_sample << std::endl;
+        abort();
+    }
+
+    auto uint8_input = wave_data.stream;
+    auto double_input = convert_uint8_to_double_stream(uint8_input);
+    auto wave = interpolate(double_input, wave_data.sample_rate);
+    auto result_wave = wave;
+
+    auto double_samples = normalise(sample_wave(result_wave, 44100));
+    auto int16_samples = convert_double_to_int16_stream(double_samples);
+    auto output_stream = convert_int16_to_uint8_stream(int16_samples);
+
+    wave_data.bits_per_sample = 16;
+    wave_data.n_channels = 1;
+    wave_data.sample_rate = 44100;
+    wave_data.stream = output_stream;
+
+    write_wave_file("e:/temp/wave/output.wav", wave_data);
+}
+
+void convert_16bit_file()
+{
+    WAVE_DATA wave_data;
+
+    read_wave_file("e:/temp/wave/input-16bit-44-mono.wav", &wave_data);
+
+    if (wave_data.n_channels != 1 || wave_data.sample_rate != 44100 || wave_data.bits_per_sample != 16)
+    {
+        std::cerr << "Unsupported format" << std::endl;
+        std::cerr << "Channels: " << wave_data.n_channels << std::endl;
+        std::cerr << "Sample rate: " << wave_data.sample_rate << std::endl;
+        std::cerr << "Bits per sample: " << wave_data.bits_per_sample << std::endl;
+        abort();
+    }
+
+    auto uint8_input = wave_data.stream;
+    auto int16_input = convert_uint8_to_int16_stream(uint8_input);
+    auto double_input = convert_int16_to_double_stream(int16_input);
+    auto wave = interpolate(double_input, wave_data.sample_rate);
+    auto result_wave = wave;
+
+    auto double_samples = normalise(sample_wave(result_wave, 44100));
+    auto int16_samples = convert_double_to_int16_stream(double_samples);
+    auto output_stream = convert_int16_to_uint8_stream(int16_samples);
+
+    wave_data.bits_per_sample = 16;
+    wave_data.n_channels = 1;
+    wave_data.sample_rate = 44100;
+    wave_data.stream = output_stream;
+
+    write_wave_file("e:/temp/wave/output.wav", wave_data);
+}
+
+int main()
+{
+    // bach_mono();
+    // bach_stereo();
+    convert_16bit_file();
+
+    std::cout << "Done!" << std::endl;
+}
+#endif
---
+++ labs/sound/solution/streams/conversions.cpp
@@ -0,0 +1,142 @@
+#include "conversions.h"
+#include <assert.h>
+
+
+namespace
+{
+    class DoubleToInt16Converter : public Stream<int16_t>
+    {
+        std::shared_ptr<Stream<double>> m_stream;
+
+    public:
+        DoubleToInt16Converter(std::shared_ptr<Stream<double>> stream)
+            : m_stream(stream) { }
+
+        unsigned size() const override
+        {
+            return m_stream->size();
+        }
+
+        int16_t operator [](unsigned index) const override
+        {
+            double value = (*m_stream)[index];
+
+            assert(-1 <= value);
+            assert(value <= 1);
+
+            return (int16_t)((*m_stream)[index] * 32767);
+        }
+    };
+
+    class Uint8ToInt16Converter : public Stream<int16_t>
+    {
+        std::shared_ptr<Stream<uint8_t>> m_stream;
+
+    public:
+        Uint8ToInt16Converter(std::shared_ptr<Stream<uint8_t>> stream)
+            : m_stream(stream) { }
+
+        unsigned size() const override
+        {
+            return m_stream->size() / 2;
+        }
+
+        int16_t operator [](unsigned index) const override
+        {
+            uint8_t lower = (*m_stream)[2 * index];
+            uint8_t upper = (*m_stream)[2 * index + 1];
+            uint16_t unsigned_combined = (upper << 8) | lower;
+            int16_t& signed_combined = *reinterpret_cast<int16_t*>(&unsigned_combined);
+
+            return signed_combined;
+        }
+    };
+
+    class Int16ToUint8Converter: public Stream<uint8_t>
+    {
+        std::shared_ptr<Stream<int16_t>> m_stream;
+
+    public:
+        Int16ToUint8Converter(std::shared_ptr<Stream<int16_t>> stream)
+            : m_stream(stream) { }
+
+        unsigned size() const override
+        {
+            return m_stream->size() * 2;
+        }
+
+        uint8_t operator [](unsigned index) const override
+        {
+            unsigned i = index / 2;
+            int16_t item = (*m_stream)[i];
+            uint16_t& unsigned_item = *reinterpret_cast<uint16_t*>(&item);
+            uint8_t lower = unsigned_item & 0xFF;
+            uint8_t upper = unsigned_item >> 8;
+
+            return index % 2 == 0 ? lower : upper;
+        }
+    };
+
+    class Int16ToDoubleConverter : public Stream<double>
+    {
+        std::shared_ptr<Stream<int16_t>> m_stream;
+
+    public:
+        Int16ToDoubleConverter(std::shared_ptr<Stream<int16_t>> stream)
+            : m_stream(stream) { }
+
+        unsigned size() const override
+        {
+            return m_stream->size();
+        }
+
+        double operator [](unsigned index) const override
+        {
+            return (1 + 2 * (*m_stream)[index]) / 65535.0;
+        }
+    };
+
+    class Uint8ToDoubleConverter : public Stream<double>
+    {
+        std::shared_ptr<Stream<uint8_t>> m_stream;
+
+    public:
+        Uint8ToDoubleConverter(std::shared_ptr<Stream<uint8_t>> stream)
+            : m_stream(stream) { }
+
+        unsigned size() const override
+        {
+            return m_stream->size();
+        }
+
+        double operator [](unsigned index) const override
+        {
+            return (*m_stream)[index] / 127.5 - 1;
+        }
+    };
+}
+
+std::shared_ptr<Stream<int16_t>> convert_double_to_int16_stream(std::shared_ptr<Stream<double>> stream)
+{
+    return std::make_shared<DoubleToInt16Converter>(stream);
+}
+
+std::shared_ptr<Stream<int16_t>> convert_uint8_to_int16_stream(std::shared_ptr<Stream<uint8_t>> stream)
+{
+    return std::make_shared<Uint8ToInt16Converter>(stream);
+}
+
+std::shared_ptr<Stream<uint8_t>> convert_int16_to_uint8_stream(std::shared_ptr<Stream<int16_t>> stream)
+{
+    return std::make_shared<Int16ToUint8Converter>(stream);
+}
+
+std::shared_ptr<Stream<double>> convert_int16_to_double_stream(std::shared_ptr<Stream<int16_t>> stream)
+{
+    return std::make_shared<Int16ToDoubleConverter>(stream);
+}
+
+std::shared_ptr<Stream<double>> convert_uint8_to_double_stream(std::shared_ptr<Stream<uint8_t>> stream)
+{
+    return std::make_shared<Uint8ToDoubleConverter>(stream);
+}
