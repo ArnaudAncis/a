4b9eb047bfba5eaf46ad946fd30792c7f860aa16
Frederic Vogels <frederic.vogels@gmail.com>
2017-04-18 21:02:20 +0200

    labs/sound

--- labs/sound/code/streams/converters/conversions.cpp
+++ labs/sound/code/streams/converters/conversions.cpp
@@ -28,30 +28,7 @@ namespace
         }
     };
 
-    class Int16ToUint8Converter : public Stream<uint8_t>
-    {
-        std::shared_ptr<Stream<int16_t>> m_stream;
-
-    public:
-        Int16ToUint8Converter(std::shared_ptr<Stream<int16_t>> stream)
-            : m_stream(stream) { }
-
-        unsigned size() const override
-        {
-            return m_stream->size() * 2;
-        }
-
-        uint8_t operator [](unsigned index) const override
-        {
-            unsigned i = index / 2;
-            int16_t item = (*m_stream)[i];
-            uint16_t& unsigned_item = *reinterpret_cast<uint16_t*>(&item);
-            uint8_t lower = unsigned_item & 0xFF;
-            uint8_t upper = unsigned_item >> 8;
-
-            return index % 2 == 0 ? lower : upper;
-        }
-    };
+    
 
     class Uint8ToDoubleConverter : public Stream<double>
     {
@@ -78,13 +55,6 @@ std::shared_ptr<Stream<int16_t>> convert_uint8_to_int16_stream(std::shared_ptr<S
 {
     return std::make_shared<Uint8ToInt16Converter>(stream);
 }
-
-std::shared_ptr<Stream<uint8_t>> convert_int16_to_uint8_stream(std::shared_ptr<Stream<int16_t>> stream)
-{
-    return std::make_shared<Int16ToUint8Converter>(stream);
-}
-
-
 
 std::shared_ptr<Stream<double>> convert_uint8_to_double_stream(std::shared_ptr<Stream<uint8_t>> stream)
 {
