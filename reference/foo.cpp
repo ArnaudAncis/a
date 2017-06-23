a9cf52592646ba07af26eb67ac7119740dbb89d0
Frederic Vogels <frederic.vogels@ucll.be>
2017-06-23 19:45:21 +0000

    removed reference

--- reference/foo.cpp
+++
@@ -1,8 +0,0 @@
-#include "foo.h"
-#include "a.h"
-#include "b.h"
-#include <lib1>
-#include <lib2>
-
-// Definitions related to foo
-// Make use of a and b, lib1, lib2
--- reference/nullcheck.cpp
+++
@@ -1,6 +0,0 @@
-#ifndef NDEBUG
-#define NULLCHECK(p) \
-  if ( !p ) abort( #p " should not be zero" )
-#else
-#define NULLCHECK(p)
-#endif
